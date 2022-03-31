bb0a943aef2266a583956f0e1c8a77f50bae3683
cartiss <kirill.fesiuk@gmail.com>
2022-03-31 16:05:33 +0300

    Result

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -125,8 +125,8 @@ REST_FRAMEWORK = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kirill.fesiuk@gmail.com'
-EMAIL_HOST_PASSWORD = 'fyxquwixeljeoqsy'
+EMAIL_HOST_USER = '**********************'
+EMAIL_HOST_PASSWORD = '*********'
 EMAIL_PORT = '465'
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
